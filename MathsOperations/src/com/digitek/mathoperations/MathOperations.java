0caf6dd98fd41a6a06c2017fc01ad704acf668b7
kunalsondhi <kunalsondhi123@gmail.com>
2016-01-06 21:31:02 -0600

    Math Operations
    Math operations

---
+++ MathsOperations/src/com/digitek/mathoperations/MathOperations.java
@@ -0,0 +1,36 @@
+package com.digitek.mathoperations;
+
+public class MathOperations {
+	
+	public static void addNumber() {
+		
+		int a= 8;
+		int b= 10;
+		
+		int c= a+b;
+		
+		System.out.println (c);
+		
+				
+	}
+	
+	public static void subtractNumber() {
+		
+		int a= 10;
+		int b= 105;
+		
+		int c= a-b;
+		
+		System.out.println(c);
+		
+	}
+	
+	public static void main(String arg[]) {
+		
+		addNumber();
+		subt ractNumber();
+		
+	}
+	
+	
+}
